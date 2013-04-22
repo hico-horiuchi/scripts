cb87310aded18a81d36a26a3dc70141243c0efc9
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-04-22 15:17:30 +0900

    変数にダブルクォートを付加

--- gcc_o.sh
+++ gcc_o.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ -e $1 ]
+if [ -e "$1" ]
 then
-    NAME=`basename $1 .c`
-    gcc -o $NAME $NAME.c
+    NAME=`basename "$1" .c`
+    gcc -o "$NAME" "$NAME.c"
 else
     echo "$1: No such file or directory"
 fi
--- tex2pdf.sh
+++ tex2pdf.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-if [ -e $1 ]
+if [ -e "$1" ]
 then
-    NAME=`basename $1 .tex`
-    nkf -w --overwrite $NAME.tex
-    platex $NAME.tex
-    dvipdfmx $NAME.dvi
-    if [ -e $NAME.aux ] ; then rm $NAME.aux; fi
-    if [ -e $NAME.dvi ] ; then rm $NAME.dvi; fi
-    if [ -e $NAME.log ] ; then rm $NAME.log; fi
-    if [ -e $NAME.nav ] ; then rm $NAME.nav; fi
-    if [ -e $NAME.out ] ; then rm $NAME.out; fi
-    if [ -e $NAME.snm ] ; then rm $NAME.snm; fi
-    if [ -e $NAME.toc ] ; then rm $NAME.toc; fi
-    if [ -e $NAME.vrb ] ; then rm $NAME.vrb; fi
-    evince $NAME.pdf
+    NAME=`basename "$1" .tex`
+    nkf -w --overwrite "$NAME.tex"
+    platex "$NAME.tex"
+    dvipdfmx "$NAME.dvi"
+    if [ -e "$NAME.aux" ] ; then rm "$NAME.aux"; fi
+    if [ -e "$NAME.dvi" ] ; then rm "$NAME.dvi"; fi
+    if [ -e "$NAME.log" ] ; then rm "$NAME.log"; fi
+    if [ -e "$NAME.nav" ] ; then rm "$NAME.nav"; fi
+    if [ -e "$NAME.out" ] ; then rm "$NAME.out"; fi
+    if [ -e "$NAME.snm" ] ; then rm "$NAME.snm"; fi
+    if [ -e "$NAME.toc" ] ; then rm "$NAME.toc"; fi
+    if [ -e "$NAME.vrb" ] ; then rm "$NAME.vrb"; fi
+    evince "$NAME.pdf"
 else
-    echo $1: No such file or directory
+    echo "$1: No such file or directory"
 fi
