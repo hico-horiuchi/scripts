d40e60fe15002f7fabc171023d75916fb5de6d77
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-04-23 19:14:21 +0900

    引数でディレクトリを指定

--- bytecompile_dir.sh
+++ bytecompile_dir.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+if [ ! -d $1 ]; then
+    echo "$1: No such directory"
+    exit 1
+fi
+
+cd $1
 _LISPDIRS=`find . -maxdepth 1 -mindepth 1 -type d`
 
 for _DIRS in ${_LISPDIRS}; do
--- git_pull_dir.sh
+++ git_pull_dir.sh
@@ -2,6 +2,12 @@
 # 手抜きgit更新スクリプト | GoingMyWay
 # http://goingmyway.net/?p=632
 
+if [ ! -d $1 ]; then
+    echo "$1: No such directory"
+    exit 1
+fi
+
+cd $1
 _GITDIRS=`find . -type d -name ".git" | sed "s/\(\.\/.*\)\/.git/\1/g"`
 
 for _DIRS in ${_GITDIRS}; do
