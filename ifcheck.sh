c7cf6db432e099c4a6f8e13ba5992df9cf203b64
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-06-12 20:58:24 +0900

    ifcheckの処理方法を変更
    - 変数を使わずに終了ステータスで無線LANを判別
    - headとtailではなくsedでIPアドレスを抽出

--- ifcheck.sh
+++ ifcheck.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-_WLAN_FLAG=`ifconfig | grep wlan0`
+ifconfig wlan0 > /dev/null 2> /dev/null
 
-if [ -n "$_WLAN_FLAG" ]; then
-    _IP_ADDRESS=`ifconfig | grep inet | head -3 | tail -1 | cut -d: -f2 | awk '{print $1;}'`
+if [ $? = "0" ]; then
+    _IP_ADDRESS=`ifconfig wlan0 | sed -n '2p' | cut -d: -f2 | awk '{print $1;}'`
     echo $_IP_ADDRESS
 else
-    _IP_ADDRESS=`ifconfig | grep inet | head -1 | cut -d: -f2 | awk '{print $1;}'`
+    _IP_ADDRESS=`ifconfig       | sed -n '2p' | cut -d: -f2 | awk '{print $1;}'`
     echo $_IP_ADDRESS
 fi
