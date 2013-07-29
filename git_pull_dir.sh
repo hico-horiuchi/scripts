727e77bd494359b1ab641af41200bd08bc33508f
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-07-29 14:12:56 +0900

    不要な中括弧を削除

--- git_pull_dir.sh
+++ git_pull_dir.sh
@@ -13,12 +13,12 @@ _PATH=`readlink -f $1`
 cd $_PATH
 _GITDIRS=`find . -type d -name ".git" | sed "s/\(\.\/.*\)\/.git/\1/g"`
 
-for _DIRS in ${_GITDIRS}; do
-    echo -e "\n-- ${_DIRS} --"
+for _DIRS in $_GITDIRS; do
+    echo -e "\n-- $_DIRS --"
     cd $_DIRS
     _PULL=`git pull`
     echo -e $_PULL
-    if [ "${_PULL}" != "Already up-to-date." ]; then
+    if [ "$_PULL" != "Already up-to-date." ]; then
         rm -f *.elc
         emacs -batch -f batch-byte-compile *.el
         echo `date  +"%Y/%m/%d/ %H:%M"` $_DIRS >> $_PATH/$_LOG
