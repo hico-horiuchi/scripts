702e321691c4ea06fc44bdc1ab3f2fe900c7dd83
Akihiko Horiuchi <12ff5b8@gmail.com>
2013-07-29 14:07:32 +0900

    絶対パスを使う

--- git_pull_dir.sh
+++ git_pull_dir.sh
@@ -2,25 +2,26 @@
 # 手抜きgit更新スクリプト | GoingMyWay
 # http://goingmyway.net/?p=632
 
-_GITLOG="git_pull.log"
+_LOG="git_pull.log"
 
 if [ ! -d $1 ]; then
     echo "$1: No such directory"
     exit 1
 fi
 
-cd $1
+_PATH=`readlink -f $1`
+cd $_PATH
 _GITDIRS=`find . -type d -name ".git" | sed "s/\(\.\/.*\)\/.git/\1/g"`
 
 for _DIRS in ${_GITDIRS}; do
     echo -e "\n-- ${_DIRS} --"
     cd $_DIRS
     _PULL=`git pull`
-    echo $_PULL
+    echo -e $_PULL
     if [ "${_PULL}" != "Already up-to-date." ]; then
         rm -f *.elc
         emacs -batch -f batch-byte-compile *.el
-        echo `date  +"%Y/%m/%d/ %H:%M"` $_DIRS >> ../$_GITLOG
+        echo `date  +"%Y/%m/%d/ %H:%M"` $_DIRS >> $_PATH/$_LOG
     fi
-    cd ../
+    cd $_PATH
 done
