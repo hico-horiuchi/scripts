a8038e13477ac05fe47bc0c23b7c051c9b3982ce
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-01-28 19:10:44 +0900

    卒論用のコマンドを追加

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -3,6 +3,12 @@ if [ -e "$1" ]
 then
     NAME=`basename "$1" .tex`
     nkf -w --overwrite "$NAME.tex"
+    if [ -e "$NAME.tex" ] ; then
+      sed -i 's/\\begin{document}//g' thesis.tex
+      sed -i 's/\\maketitle//g' thesis.tex
+      sed -i 's/\\end{keyword}/\\end{keyword}\\begin{document}\\maketitle/g' thesis.tex
+    fi
+    platex "$NAME.tex"
     platex "$NAME.tex"
     dvipdfmx -f $HOME/.fonts/kozuka.map "$NAME.dvi"
     if [ -e "$NAME.dvi" ] ; then rm "$NAME.dvi"; fi
