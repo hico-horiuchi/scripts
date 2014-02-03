b65cccdfe587dd69c2b4df36bc8dfa84eb70d145
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-02-03 15:32:41 +0900

    卒論用のコマンドを追加

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -7,6 +7,7 @@ then
       sed -i 's/\\begin{document}//g' thesis.tex
       sed -i 's/\\maketitle//g' thesis.tex
       sed -i 's/\\end{keyword}/\\end{keyword}\\begin{document}\\maketitle/g' thesis.tex
+      sed -i 's/\.9\\linewidth/\.7\\linewidth/g' thesis.tex
     fi
     platex "$NAME.tex"
     platex "$NAME.tex"
