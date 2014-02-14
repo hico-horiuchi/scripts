79cad2d1e9dff7f78a4980caea955f555789dafe
Akihiko Horiuchi <12ff5b8@gmail.com>
2014-02-14 11:17:31 +0900

    変数の展開を修正

--- tex2pdf.sh
+++ tex2pdf.sh
@@ -3,16 +3,18 @@ if [ -e "$1" ]
 then
     NAME=`basename "$1" .tex`
     nkf -w --overwrite "$NAME.tex"
-    if [ -e "$NAME.tex" ] ; then
-      sed -i 's/\\begin{document}//g' thesis.tex
-      sed -i 's/\\maketitle//g' thesis.tex
-      sed -i 's/\\end{keyword}/\\end{keyword}\\begin{document}\\maketitle/g' thesis.tex
-      sed -i 's/\.9\\linewidth/\.7\\linewidth/g' thesis.tex
-      sed -i 's/\(\\includegraphics.*_graph\.eps}\)/\\fbox{\1}/g' thesis.tex
+    if [ "$NAME" = 'thesis' ] ; then
+      sed -i 's/\\begin{document}//g' "$NAME.tex"
+      sed -i 's/\\maketitle//g' "$NAME.tex"
+      sed -i 's/\\end{keyword}/\\end{keyword}\\begin{document}\\maketitle/g' "$NAME.tex"
+      sed -i 's/\[htb\]/\[ht\]/g' "$NAME.tex"
+      sed -i 's/\.9\\linewidth/\.7\\linewidth/g' "$NAME.tex"
+      sed -i 's/\.7\\linewidth\]{..\/figure\/expr_r/\.9\\linewidth\]{..\/figure\/expr_r/g' "$NAME.tex"
+      sed -i 's/\(\\includegraphics.*_graph\.eps}\)/\\fbox{\1}/g' "$NAME.tex"
     fi
     platex "$NAME.tex"
     platex "$NAME.tex"
-    dvipdfmx -f $HOME/.fonts/kozuka.map "$NAME.dvi"
+    dvipdfmx -f "$HOME/.fonts/kozuka.map" "$NAME.dvi"
     if [ -e "$NAME.dvi" ] ; then rm "$NAME.dvi"; fi
     if [ -e "$NAME.log" ] ; then rm "$NAME.log"; fi
     if [ -e "$NAME.nav" ] ; then rm "$NAME.nav"; fi
@@ -20,7 +22,7 @@ then
     if [ -e "$NAME.snm" ] ; then rm "$NAME.snm"; fi
     if [ -e "$NAME.toc" ] ; then rm "$NAME.toc"; fi
     if [ -e "$NAME.vrb" ] ; then rm "$NAME.vrb"; fi
-    evince "$NAME.pdf"
+    if [ -e "$NAME.pdf" ] ; then evince "$NAME.pdf" ; fi
 else
     echo "$1: No such file or directory"
 fi
