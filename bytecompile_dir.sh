953e867dc1f9fb860a03c8a72ae778c324937a56
Akihiko Horiuchi <s14g481@stmail.eng.kagawa-u.ac.jp>
2014-12-13 22:44:12 +0900

    Update bytecompile_dir.sh

--- bytecompile_dir.sh
+++ bytecompile_dir.sh
@@ -5,13 +5,19 @@ if [ ! -d $1 ]; then
     exit 1
 fi
 
-cd $1
+_PATH=`readlink -f $1`
+cd $_PATH
 _LISPDIRS=`find . -maxdepth 1 -mindepth 1 -type d`
 
 for _DIRS in ${_LISPDIRS}; do
-    echo -e "\n-- ${_DIRS} --"
     cd $_DIRS
+    if [ -f "Makefile" ]; then
+        cd $PATH
+        continue
+    fi
+
+    echo -e "\n-- ${_DIRS} --"
     rm -f *.elc
     emacs -batch -f batch-byte-compile *.el
-    cd ../
+    cd $PATH
 done
